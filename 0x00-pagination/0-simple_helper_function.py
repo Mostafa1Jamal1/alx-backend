0933656d1e8aa3e85a0a7d8cb444d3b294d85059
mostafa_p_18 <mostafa.jamal.mjm@gmail.com>
2024-05-20 11:56:07 +0300

    task 0: Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' Simple helper function '''
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    ''' return a tuple of size two containing
+    a start index and an end index corresponding to the range of indexes '''
+    return tuple([(page - 1) * page_size, page * page_size])
