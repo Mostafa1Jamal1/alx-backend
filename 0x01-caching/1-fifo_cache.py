56abed88046639f7f69ab5e6b13454e84648e6c2
mostafa_p_18 <mostafa.jamal.mjm@gmail.com>
2024-08-20 17:45:29 +0300

    task 1 in 0x01-caching project (FIFO caching)

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""FIFOCache Module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Defines FIFO caching module
+    """
+
+    def __init__(self):
+        """Initiliaze
+        """
+        super().__init__()
+        self.keys = list()
+
+    def put(self, key, item):
+        """Assign item to cache using FIFO replacement policy.
+        """
+        data = self.cache_data
+        if key and item:
+            if not key in data and len(data) >= self.MAX_ITEMS:
+                self.cache_data.pop(self.keys[0])
+                print("DISCARD: {}".format(self.keys[0]))
+                self.keys.pop(0)
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """Return value linked to key from cache.
+        """
+        return self.cache_data.get(key)
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -2,3 +2,5 @@
 
 
 `*0-basic_cache.py*` -> a class BasicCache that inherits from BaseCaching and is a caching system.
+
+`*1-fifo_cache.py*` -> a class FIFOCache that inherits from BaseCaching and is a caching system
