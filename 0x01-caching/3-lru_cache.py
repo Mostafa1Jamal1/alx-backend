7b27e18ab10821e30b15df9c72168b7ce08597d3
mostafa_p_18 <mostafa.jamal.mjm@gmail.com>
2024-08-20 23:24:27 +0300

    task 3 in 0x01-caching project (LRU Caching)

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,38 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""FIFOCache Module
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Defines LRU caching module
+    """
+
+    def __init__(self):
+        """Initiliaze
+        """
+        super().__init__()
+        self.keys = list()
+
+    def put(self, key, item):
+        """Assign item to cache using LIFO replacement policy.
+        """
+        data = self.cache_data
+        if key and item:
+            if key not in data and len(data) >= self.MAX_ITEMS:
+                self.cache_data.pop(self.keys[0])
+                print("DISCARD: {}".format(self.keys[0]))
+                self.keys.pop(0)
+            if key in self.keys:
+                self.keys.remove(key)
+            self.keys.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return value linked to key from cache.
+        """
+        if key in self.keys:
+            self.keys.remove(key)
+            self.keys.append(key)
+        return self.cache_data.get(key)
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -6,3 +6,5 @@
 `*1-fifo_cache.py*` -> a class FIFOCache that inherits from BaseCaching and is a caching system
 
 `*2-lifo_cache.py*` -> a class LIFOCache that inherits from BaseCaching and is a caching system
+
+`*3-lru_cache.py*` -> a class LRUCache that inherits from BaseCaching and is a caching system
