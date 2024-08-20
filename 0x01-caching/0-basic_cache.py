a7cdb47d1436982245741ae2398d48910c54abcf
mostafa_p_18 <mostafa.jamal.mjm@gmail.com>
2024-08-20 15:45:06 +0300

    task 0 in 0x01-caching project (Basic dictionary)

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""BasicCache Module
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Defines basic caching module
+    """
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+        the item value for the key key.
+        If key or item is None, this method will do nothing.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key.
+        """
+        return self.cache_data.get(key)
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -1,1 +1,4 @@
-0x01-caching
+# 0x01-caching Project
+
+
+`*0-basic_cache.py*` -> a class BasicCache that inherits from BaseCaching and is a caching system.
