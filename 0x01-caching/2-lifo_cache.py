6058a1207f6d1afc5a93f2c11303551fdcbc1da2
mostafa_p_18 <mostafa.jamal.mjm@gmail.com>
2024-08-20 18:15:48 +0300

    task 2 in 0x01-caching project (LIFO Caching)

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""FIFOCache Module
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Defines LIFO caching module
+    """
+
+    def __init__(self):
+        """Initiliaze
+        """
+        super().__init__()
+        self.keys = list()
+
+    def put(self, key, item):
+        """Assign item to cache using LIFO replacement policy.
+        """
+        data = self.cache_data
+        if key and item:
+            if key not in data and len(data) >= self.MAX_ITEMS:
+                self.cache_data.pop(self.keys[-1])
+                print("DISCARD: {}".format(self.keys.pop()))
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """Return value linked to key from cache.
+        """
+        return self.cache_data.get(key)
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -4,3 +4,5 @@
 `*0-basic_cache.py*` -> a class BasicCache that inherits from BaseCaching and is a caching system.
 
 `*1-fifo_cache.py*` -> a class FIFOCache that inherits from BaseCaching and is a caching system
+
+`*2-lifo_cache.py*` -> a class LIFOCache that inherits from BaseCaching and is a caching system
