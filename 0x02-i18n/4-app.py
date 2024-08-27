9319fded1d72d1362363cff120f3b127edfc559e
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-08-28 02:44:45 +0300

    4. Force locale with URL parameter in 0x02-i18n project

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Flask app for learing I18n
+"""
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+)
+from flask_babel import Babel, _
+
+
+class Config:
+    """config class for babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ to determine the best match with our supported languages.
+    """
+    if request.args.get('locale') is not None:
+        return request.args.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+@app.route("/<lang>")
+def hello_world():
+    """home route for a basic page
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
