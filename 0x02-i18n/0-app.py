230ac0e2e015c92d362eab5d936afe342bf0781e
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-08-27 21:39:09 +0300

    task 0 in 0x02-i18n project (Basic Flask app)

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Flask app for learing I18n
+"""
+
+from flask import (
+    Flask,
+    render_template,
+)
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template('0-index.html')
