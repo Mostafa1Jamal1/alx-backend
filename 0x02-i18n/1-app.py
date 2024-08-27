1f959f606b6a83b698b495f835af99f3efdd8be6
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-08-27 22:31:36 +0300

    1. Basic Babel setup in 0x02-i18n project

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Flask app for learing I18n
+"""
+
+from flask import (
+    Flask,
+    render_template,
+)
+from flask_babel import Babel
+
+
+class Config():
+    """config class for babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_LOCALE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hello_world():
+    """home route for a basic page
+    """
+    return render_template('1-index.html')
