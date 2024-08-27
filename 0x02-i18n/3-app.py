97ff0a860bae6ff78017b6ccf359e117c3a53577
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-08-28 02:05:47 +0300

    3. Parametrize templates in 0x02-i18n project

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,1 +1,41 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Flask app for learing I18n
+"""
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+)
+from flask_babel import Babel, _
+
+
+class Config:
+    """config class for babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ to determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def hello_world():
+    """home route for a basic page
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
