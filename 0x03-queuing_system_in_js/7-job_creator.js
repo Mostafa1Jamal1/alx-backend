87e2af1d6c503bfb0b1aa4cb8f068d7bf8b7ab50
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-24 22:23:24 +0300

    8. Track progress and errors with Kue: Create the Job creator

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -1,1 +1,63 @@
-#!/usr/bin/node
+import kue from 'kue';
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account'
+  }
+];
+const queue = kue.createQueue();
+
+for (const job of jobs) {
+  const Job = queue
+    .create('push_notification_code_2', job)
+    .save(err => {
+      if (!err) console.log(`Notification job created: ${Job.id}`);
+    }).on('complete', () => {
+      console.log(`Notification job ${Job.id} completed`);
+    }).on('failed', (err) => {
+      console.log(`Notification job ${Job.id} failed: ${err}`);
+    }).on('progress', function (progress, data) {
+      console.log(`Notification job ${Job.id} ${progress}% complete`);
+    });
+}
