725feb68582ddbc4264160a978e16aa6a8ca70dd
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-24 20:27:48 +0300

    6. Create the Job creator

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+import kue from 'kue';
+
+const queue = kue.createQueue();
+const Job = queue.create('push_notification_code', {
+  phoneNumber: '1234567890',
+  message: 'jon message',
+}).save(err => {
+  if (!err) console.log(`Notification job created: ${Job.id}`);
+});
+
+Job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+})
