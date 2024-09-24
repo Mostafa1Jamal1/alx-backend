8cc0b829edfcf51ff4e186b02371879ce25294ea
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-24 19:45:49 +0300

    5. Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,1 +1,23 @@
-#!/usr/bin/node
+import redis from 'redis';
+const createClient = redis.createClient
+
+const subscriber = createClient()
+subscriber
+  .on('error', err => {
+    console.log(`Redis client not connected to the server: ${err}`)
+  })
+  .on('connect', () => {
+    console.log('Redis client connected to the server')
+  })
+
+subscriber.subscribe('holberton school channel');
+
+subscriber.on('message', (channel, message) => {
+  if (channel === 'holberton school channel') {
+    console.log(`${message}`)
+    if (message === 'KILL_SERVER') {
+      subscriber.unsubscribe()
+      subscriber.quit()
+    }
+  }
+})
