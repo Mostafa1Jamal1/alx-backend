6165f7c34728b96e3b3a6532313b77b71b265229
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-09-25 20:31:43 +0300

    11. Writing the test for job creation

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -4,16 +4,19 @@ const createPushNotificationsJobs = function (jobs, queue) {
   }
   for (const job of jobs) {
     const Job = queue
-      .create('push_notification_code_3', job)
-      .save(err => {
-        if (!err) console.log(`Notification job created: ${Job.id}`);
-      }).on('complete', () => {
-        console.log(`Notification job ${Job.id} completed`);
-      }).on('failed', (err) => {
-        console.log(`Notification job ${Job.id} failed: ${err}`);
-      }).on('progress', function (progress, data) {
-        console.log(`Notification job ${Job.id} ${progress}% complete`);
-      });
+      .create('push_notification_code_3', job);
+    Job.save(err => {
+      if (!err) console.log(`Notification job created: ${Job.id}`);
+    });
+    Job.on('complete', () => {
+      console.log(`Notification job ${Job.id} completed`);
+    });
+    Job.on('failed', (err) => {
+      console.log(`Notification job ${Job.id} failed: ${err}`);
+    });
+    Job.on('progress', function (progress, data) {
+      console.log(`Notification job ${Job.id} ${progress}% complete`);
+    });
   }
 }
 
--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,1 +1,65 @@
-#!/usr/bin/node
+import createPushNotificationsJobs from './8-job.js';
+import kue from 'kue';
+import chai from 'chai';
+import sinon from 'sinon';
+
+const expect = chai.expect;
+const queue = kue.createQueue();
+
+describe('createPushNotificationsJobs', () => {
+  before(() => {
+    queue.testMode.enter();
+  })
+  beforeEach(() => {
+    sinon.spy(console, 'log');
+  })
+  afterEach(() => {
+    queue.testMode.clear();
+    sinon.restore();
+  })
+  after(() => {
+    queue.testMode.exit();
+  })
+
+  it('should create jobs and log them', () => {
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 4562 to verify your account'
+      }];
+    createPushNotificationsJobs(jobs, queue);
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(console.log.calledWith('Notification job created: ' + queue.testMode.jobs[0].id)).to.be.true;
+    expect(console.log.calledWith('Notification job created: ' + queue.testMode.jobs[1].id)).to.be.true;
+  });
+
+  it('should create jobs of type push_notification_code_3 with the correct data', () => {
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 4562 to verify your account'
+      }];
+    createPushNotificationsJobs(jobs, queue);
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data.phoneNumber).to.equal('4153518780');
+    expect(queue.testMode.jobs[1].data.phoneNumber).to.equal('4153518781');
+  });
+
+  it('should display error if jobs is not an array', () => {
+    const queue = kue.createQueue();
+    expect(function () { createPushNotificationsJobs('jobs', queue) })
+      .to.throw(Error, 'Jobs is not an array');
+    expect(queue.testMode.jobs.length).to.equal(0);
+  });
+
+})
